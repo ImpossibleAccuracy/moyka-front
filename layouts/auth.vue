b0da95c9eb874e654cfac3d296103b5996f6b950
ImpossibleAccuracy <rvarzin1@gmail.com>
2025-04-25 19:03:15 +0500

    fix ui issues

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-window relative w-screen h-screen flex center px-8 py-4">
     <Button v-slot="slotProps" as-child>
-      <RouterLink class="absolute top-4 left-8 max-lg:hidden" to="/" :class="slotProps.class">
+      <RouterLink class="!absolute top-4 left-8 max-lg:hidden" to="/" :class="slotProps.class">
         На главную
       </RouterLink>
     </Button>
